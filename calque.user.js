739c0221a961249e8d8e28eb478fd7967cdce57a
zps4sjvvc9-glitch <zps4sjvvc9@privaterelay.appleid.com>
2026-09-16 00:58:09 +0200

    Update calque.user.js

--- calque.user.js
+++ calque.user.js
@@ -22,7 +22,7 @@
         const calque = document.createElement('img');
         
         const timestamp = new Date().getTime();
-        calque.src = 'https://raw.githubusercontent.com/zps4sjvvc9-glitch/CdB/main/calque.png?v=' + timestamp; 
+        calque.src = 'https://raw.githubusercontent.com/zps4sjvvc9-glitch/CdB/main/calque.png?v='; 
         
         calque.style.position = 'absolute';
         calque.style.top = '0';
